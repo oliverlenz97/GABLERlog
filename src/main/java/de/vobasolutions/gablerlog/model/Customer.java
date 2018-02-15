af5ba447b3a6297a2f2b6bafb482abdf59846406
Oliver Lenz <oli.lenz@icloud.com>
2018-02-15 18:01:11 +0100

    Letzter Test vor dem Start

--- src/main/java/de/vobasolutions/gablerlog/model/Customer.java
+++ src/main/java/de/vobasolutions/gablerlog/model/Customer.java
@@ -1,26 +1,38 @@
 package de.vobasolutions.gablerlog.model;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class Customer {
-    private String firstName;
-    private String lastName;
+    private String firstName = "John";
+    private String lastName = "Doe";
     private Integer customerId;
 
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public Integer getCustomerId() {
         return customerId;
     }
+
     public void setCustomerId(Integer customerId) {
         this.customerId = customerId;
+    }
+
+    public String showGreeting() {
+        return "Hello " + firstName + " " + lastName + "!";
     }
 }
