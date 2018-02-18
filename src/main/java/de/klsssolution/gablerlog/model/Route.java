e52cdfb86e2c4a80543c1589c1541d0b862a5dc3
Annika Schröder <annika.schroeder@iwelt.de>
2018-02-18 13:00:59 +0100

    Auftragsübersicht erstellt.

--- src/main/java/de/klsssolution/gablerlog/model/Route.java
+++ src/main/java/de/klsssolution/gablerlog/model/Route.java
@@ -1,6 +1,11 @@
 package de.klsssolution.gablerlog.model;
 
 public class Route {
+    private double distanz;
+    private int id;
+    private String startadresse;
+    private String zieladresse;
+
 
 
 }
--- src/main/java/de/klsssolution/gablerlog/presentation/KundenBean.java
+++ src/main/java/de/klsssolution/gablerlog/presentation/KundenBean.java
@@ -6,15 +6,42 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ManagedBean
 @ViewScoped
 public class KundenBean {
     private List<Kunde> kunden;
+    private List<String> firmenbezeichnungen;
+
+    public List<String> getFirmenbezeichnungen() {
+        return firmenbezeichnungen;
+    }
+
+    public void setFirmenbezeichnungen(List<String> firmenbezeichnungen) {
+        this.firmenbezeichnungen = firmenbezeichnungen;
+    }
+
+    private String gewaehlterKunde;
+    private Map<String, String> kundenMap;
+
+
+    public String getGewaehlterKunde() {
+        return gewaehlterKunde;
+    }
+
+    public void setGewaehlterKunde(String gewaehlterKunde) {
+        this.gewaehlterKunde = gewaehlterKunde;
+    }
 
     public List<Kunde> getKunden() {
         return kunden;
+    }
+
+    public Map<String, String> getKundenMap() {
+        return kundenMap;
     }
 
     @PostConstruct
@@ -22,27 +49,39 @@ public class KundenBean {
         List<Kunde> kunden = new ArrayList<Kunde>();
 
         Kunde kunde1 = new Kunde();
-        kunde1.setVorname("John");
-        kunde1.setNachname("Doe");
+        kunde1.setFirmenbezeichnung("Ecotest GmbH");
+        kunde1.setAnsprechpartner("Tim Müller");
         kunde1.setKundenId(123456);
 
         kunden.add(kunde1);
 
         Kunde kunde2 = new Kunde();
-        kunde2.setVorname("Adam");
-        kunde2.setNachname("Scott");
+        kunde2.setFirmenbezeichnung("Meier und Sohn");
+        kunde2.setAnsprechpartner("Anna Schneider");
         kunde2.setKundenId(98765);
 
         kunden.add(kunde2);
 
         Kunde kunde3 = new Kunde();
-        kunde3.setVorname("Jane");
-        kunde3.setNachname("Doe");
+        kunde3.setFirmenbezeichnung("Zara SE");
+        kunde3.setAnsprechpartner("Thomas Doe");
         kunde3.setKundenId(65432);
 
         kunden.add(kunde3);
         this.kunden = kunden;
+
+        Map<String, String> kundenMap = new HashMap<String, String>();
+        List<String> firmenbezeichnungen = new ArrayList<String>();
+        for (Kunde kunde : kunden) {
+            kundenMap.put(kunde.getFirmenbezeichnung(), kunde.getFirmenbezeichnung());
+            firmenbezeichnungen.add(kunde.getFirmenbezeichnung());
+        }
+        this.kundenMap = kundenMap;
+        this.firmenbezeichnungen = firmenbezeichnungen;
+
     }
+
+
 }
 
 //DB Daten abrufen
