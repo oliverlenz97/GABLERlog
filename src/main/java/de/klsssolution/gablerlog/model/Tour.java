86fdffbd883f15410ae091bf9cef7906ed07bcbd
Marius K <marius@kellerland.de>
2018-02-20 19:47:53 +0100

    Klasse Tour, Tourenübersicht, TourBean...

--- src/main/java/de/klsssolution/gablerlog/model/Tour.java
+++ src/main/java/de/klsssolution/gablerlog/model/Tour.java
@@ -2,6 +2,7 @@ package de.klsssolution.gablerlog.model;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
+import de.klsssolution.gablerlog.model.DBDemo;
 
 public class Tour {
     private int tourId;
--- src/main/java/de/klsssolution/gablerlog/presentation/TourBean.java
+++ src/main/java/de/klsssolution/gablerlog/presentation/TourBean.java
@@ -3,14 +3,14 @@ package de.klsssolution.gablerlog.presentation;
 import de.klsssolution.gablerlog.model.Tour;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import de.klsssolution.gablerlog.model.ZeitFormat;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class TourBean {
     private List<Tour> tours;
     private Tour selectedTour;
